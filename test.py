85e92108fc6ecb5293cc76770d98904bad316dd4
Osama Kamal-Al-Deen <osama.kamal-al-deen@ge.com>
2017-03-30 13:51:52 -0400

    changed message

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def main():
 	assert helloWorld.printName(name) == "Hello World, I am " + name
 	#assert helloWorld.changeTest() == True
 
-	print ("All assertion conditions met!")
+	print ("All assertion conditions met, msg v2!")
 	return True
 	
 if __name__ == "__main__":
