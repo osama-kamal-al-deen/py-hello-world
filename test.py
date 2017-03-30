e8f4506f96dd0b93563877fa3b4fed20ca5a4a19
Osama Kamal-Al-Deen <osama.kamal-al-deen@ge.com>
2017-03-30 10:51:17 -0400

    minor changes to test script

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import sys, helloWorld
+def main():
+	if len(sys.argv) > 1:
+		name = sys.argv[1]
+	else:
+		name = "Osama"
 
-if len(sys.argv) > 1:
-	name = sys.argv[1]
-else:
-	name = "Osama"
-
-print "attempting test with name " + name
+	print "attempting test with name " + name
 	
-assert helloWorld.printName(name) == "Hello World, I am " + name
+	assert helloWorld.printName(name) == "Hello World, I am " + name
 
-print "All assertion conditions met!"
+	print "All assertion conditions met!"
+	return True
+	
+if __name__ == "__main__":
+	main()
